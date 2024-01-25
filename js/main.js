b569ff47ea602b282903ed7f6a20c0637c9773e1
DruDruDru <borisrasgard251@gmail.com>
2024-01-25 21:32:37 +0700

    js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-
-let p = document.querySelector('.helloWorld')
-const button = document.querySelector('button')
-
-button.addEventListener('click', () => {
-    ++count;
-    p.textContent = count;
-})
